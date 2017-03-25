68d03e9cea6cecd5040cc446c32858c640acacc5
Christopher Tunnell <ctunnell@nikhef.nl>
2017-03-24 21:16:48 -0500

    Bump version: 0.9.5 → 0.10.0

--- lax/__init__.py
+++ lax/__init__.py
@@ -1,5 +1,5 @@
 """Module defining key components to lax"""
 
-__version__ = '0.9.5'
+__version__ = '0.10.0'
 
 from . import lichens
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='lax',
-    version='0.9.5',
+    version='0.10.0',
     description="Package for standardizing event selections on hax minitrees.",
     long_description=readme + '\n\n' + history,
     author="Christopher Tunnell",
