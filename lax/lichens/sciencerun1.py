78190693f00b3d156fdcef37d0b4507391cfdceb
Patrick de Perio <pdeperio@users.noreply.github.com>
2017-07-12 05:22:51 +0900

    Update S2WidthCut for SR1 (#62)
    * Created Lichen for Scienceun1. Updated AmBe FV. Created NG FV
    
    * Update S2WidthCut for SR1
    
    Reference note:
    https://xecluster.lngs.infn.it/dokuwiki/doku.php?id=xenon:xenon1t:yuehuan:analysis:1sciencerun_s2width
    
    * Update S2WidthCut for SR1 -> Try fix bug (#60)
    
    Added: @staticmethod
    
    * Update sciencerun1.py
    
    * update value with pax665 data.
    
    * try to fix function: subpre
    
    remove (self, )
    
    * try to fix bug
    
    * fix bug
    
    Copy again everything from SR0 lax,  only change the parameters.
    
    * Attempt to fix subpre problem
    
    * Created Lichen for Scienceun1. Updated AmBe FV. Created NG FV
    
    * Update S2WidthCut for SR1
    
    Reference note:
    https://xecluster.lngs.infn.it/dokuwiki/doku.php?id=xenon:xenon1t:yuehuan:analysis:1sciencerun_s2width
    
    * Update S2WidthCut for SR1 -> Try fix bug (#60)
    
    Added: @staticmethod
    
    * Update sciencerun1.py
    
    * update value with pax665 data.
    
    * try to fix function: subpre
    
    remove (self, )
    
    * try to fix bug
    
    * fix bug
    
    Copy again everything from SR0 lax,  only change the parameters.
    
    * Attempt to fix subpre problem
    
    * Rebase with master and fix link style issues
    
    * Fix lint whitespace issue
    
    * try to fix buy
    
    * try to fix 2
    
    * try to fix buy 3
    
    * try to fix bug 4
    
    * try fix buy 5
    
    * t ry to fix bug  6
    
    * try fix bug 7
    
    * hardcode S2 width parameters
    
    Hardcode drift velocity (1.335um/ns) and diffusion constant (31.73 cm^2/s) for SR1.
    
    * hardcode s2 width parameters (#63)
    
    * hardcode s2 width parameters
    
    hardcode diffusion constant and drift velocity for SR0
    
    * More precision 
    
    to minimize difference from previous version
    
    * s2 width cut, try to fix bug
    
    remove one while space after " ((units.cm)**2) / units.s"
    
    * s2 width cut (sr0), hardcode, try to fix buy
    
    remove one blank after "((units.cm)**2) / units.s"

--- lax/lichens/sciencerun1.py
+++ lax/lichens/sciencerun1.py
@@ -6,10 +6,11 @@ This includes all current definitions of the cuts for the first science run
 # -*- coding: utf-8 -*-
 import inspect
 import os
-from pax import configuration
+import numpy as np
+from pax import units, configuration
 
 PAX_CONFIG = configuration.load_configuration('XENON1T')
-from lax.lichen import ManyLichen, StringLichen
+from lax.lichen import Lichen, RangeLichen, ManyLichen, StringLichen
 from lax.lichens import sciencerun0
 from lax import __version__ as lax_version
 
@@ -125,7 +126,6 @@ class AmBeFiducial(StringLichen):
     Contact: Erik Hogenbirk <ehogenbi@nikhef.nl>
 
     Position updated to reflect correct I-Belt 1 position. Link to Note:xenon:xenon1t:analysis:dominick:sr1_ambe_check
-
     """
     version = 2
     string = "(distance_to_source < 103.5) & (-92.9 < z) & (z < -9) & (sqrt(x*x + y*y) < 42.00)"
@@ -157,8 +157,6 @@ class NGFiducial(StringLichen):
 
 InteractionExists = sciencerun0.InteractionExists
 
-InteractionPeaksBiggest = sciencerun0.InteractionPeaksBiggest
-
 S1LowEnergyRange = sciencerun0.S1LowEnergyRange
 
 S1MaxPMT = sciencerun0.S1MaxPMT
@@ -177,7 +175,64 @@ S2PatternLikelihood = sciencerun0.S2PatternLikelihood
 
 S2Threshold = sciencerun0.S2Threshold
 
-S2Width = sciencerun0.S2Width
+
+class S2Width(ManyLichen):
+    """S2 Width cut based on diffusion model
+    The S2 width cut compares the S2 width to what we could expect based on its depth in the detector. The inputs to
+    this are the drift velocity and the diffusion constant. The allowed variation in S2 width is greater at low
+    energy (since it is fluctuating statistically) Ref: (arXiv:1102.2865)
+    It should be applicable to data regardless of if it ER or NR;
+    above cS2 = 1e5 pe ERs the acceptance will go down due to track length effects.
+    Tune the diffusion model parameters based on pax v6.4.2 AmBe data according to note:
+    xenon:xenon1t:yuehuan:analysis:0sciencerun_s2width_update0#comparison_with_diffusion_model_cut_by_jelle_pax_v642
+    Contact: Yuehuan <weiyh@physik.uzh.ch>, Jelle <jaalbers@nikhef.nl>
+    """
+    version = 2
+
+    def __init__(self):
+        self.lichen_list = [self.S2WidthHigh(),
+                            self.S2WidthLow()]
+
+    @staticmethod
+    def s2_width_model(z):
+        diffusion_constant =  31.73 * ((units.cm)**2) / units.s
+        v_drift = 1.335 * (units.um) / units.ns
+        GausSigmaToR50 = 1.349
+
+        EffectivePar = 1.16
+        Sigma_0 = 229.58 * units.ns
+        return GausSigmaToR50 * np.sqrt(Sigma_0 ** 2 - EffectivePar *2 * diffusion_constant * z / v_drift ** 3)
+
+    @staticmethod
+    def subpre(self, df):
+        df.loc[:, 'temp'] = df['s2_range_50p_area'] / S2Width.s2_width_model(df['z'])
+        return df
+
+    @staticmethod
+    def relative_s2_width_bounds(s2, kind='high'):
+        x = 0.5 * np.log10(np.clip(s2, 150, 4500 if kind == 'high' else 2500))
+        if kind == 'high':
+            return 3 - x
+        elif kind == 'low':
+            return -0.9 + x
+        raise ValueError("kind must be high or low")
+
+    class S2WidthHigh(Lichen):
+        def pre(self, df):
+            return S2Width.subpre(self, df)
+
+        def _process(self, df):
+            df.loc[:, self.name()] = (df.temp <= S2Width.relative_s2_width_bounds(df.s2, kind='high'))
+            return df
+
+    class S2WidthLow(RangeLichen):
+        def pre(self, df):
+            return S2Width.subpre(self, df)
+
+        def _process(self, df):
+            df.loc[:, self.name()] = (S2Width.relative_s2_width_bounds(df.s2, kind='low') <= df.temp)
+            return df
+
 
 S1AreaFractionTop = sciencerun0.S1AreaFractionTop
 
