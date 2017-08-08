1ac8ea22206f9f0ea1bf7062dacb2f8e7afdd776
tianyu zhu <tz2263@columbia.edu>
2017-08-08 13:45:54 -0400

    Fix a bug in S2SingleScatter
    Added a pair of parentheses

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -510,7 +510,7 @@ class S2SingleScatter(Lichen):
 
     def _process(self, df):
         largest_other_s2_is_nan = np.isnan(df.largest_other_s2)
-        df.loc[:, self.name()] = largest_other_s2_is_nan | df.largest_other_s2 < self.other_s2_bound(df.s2)
+        df.loc[:, self.name()] = largest_other_s2_is_nan | (df.largest_other_s2 < self.other_s2_bound(df.s2))
         return df
 
 
