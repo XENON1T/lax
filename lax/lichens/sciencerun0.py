890b4bf9d33fd086f99f43f4aabef15c1ac56fa9
Adam Brown <adambrown3456@gmail.com>
2017-11-29 17:35:18 -0500

    Include KryptonMisIdS1 in all-energy cuts for SR0

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -43,7 +43,8 @@ class AllEnergy(ManyLichen):
             S1SingleScatter(),
             S2PatternLikelihood(),
             S2Tails(),
-            MuonVeto()
+            MuonVeto(),
+            KryptonMisIdS1()
         ]
 
 
