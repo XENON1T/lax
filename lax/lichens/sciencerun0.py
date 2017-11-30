8a6226baae8d92590aa390cf062983343f72238a
Daniel Coderre <daniel.coderre@physik.uni-freiburg.de>
2017-11-30 17:41:19 +0100

    Change docstring format for codacy

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -826,12 +826,14 @@ class MuonVeto(StringLichen):
     string = "nearest_muon_veto_trigger < -2000000 | nearest_muon_veto_trigger > 3000000"
 
 class Flash(Lichen):
-    # Cuts events within a flash. This is defined as the width were the BUSY on channel is "high".
-    # In addition an extended time-window around the flash is removed as well.
-    # The length of the extended time-window is not finalized yet
-    # Needs FlashIdentification minitree
-    # Information: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:sciencerun1:flashercut
-    # Contact: Oliver Wack <oliver.wack@mpi-hd.mpg.de>
+    """Cuts events within a flash. This is defined as the width were the BUSY on channel is "high".
+    In addition an extended time-window around the flash is removed as well.
+    The length of the extended time-window is not finalized yet
+    Needs FlashIdentification minitree
+    Information: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:sciencerun1:flashercut
+    Contact: Oliver Wack <oliver.wack@mpi-hd.mpg.de>
+    """
+
     version = 0
     def _process(self,df):
         df.loc[:, self.name()]=((df['inside_flash']== False) &
