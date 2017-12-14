987d7df8e192f754f77aa2c534db31a22220916d
Andrea Molinario <amolinar@to.infn.it>
2017-12-14 20:10:44 +0100

    MV cut update to account for the coverage (#119)
    * MV cut update to account for the coverage
    
    * Increment version and lint fixes

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -14,7 +14,6 @@ from pax import units
 from scipy.stats import chi2
 from scipy.interpolate import RectBivariateSpline
 from scipy.stats import binom_test
-from scipy import interpolate
 import json
 
 from lax.lichen import Lichen, RangeLichen, ManyLichen, StringLichen
@@ -765,13 +764,16 @@ class MuonVeto(StringLichen):
     and the nearest MV trigger.
     The event is excluded if the nearest MV trigger falls in a [-2ms,+3ms] time window
     with respect to the reference position.
+    It also excludes events when MV was not working (abs(nearest_muon_veto_trigger)>20 s).
     It requires Proximity minitrees.
     https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:mv_cut_sr1
     Contact: Andrea Molinario <andrea.molinario@lngs.infn.it>
     """
 
-    version = 1
-    string = "nearest_muon_veto_trigger < -2000000 | nearest_muon_veto_trigger > 3000000"
+    version = 2
+    string = ("(nearest_muon_veto_trigger < -2e6 & nearest_muon_veto_trigger > -2e10) | "
+              "(nearest_muon_veto_trigger > 3e6 & nearest_muon_veto_trigger < 2e10)"
+              )
 
 
 class KryptonMisIdS1(StringLichen):
@@ -818,7 +820,7 @@ class PosDiff(Lichen):
 
     def _process(self, df):
         df.loc[:, self.name()] = (((df['x_observed_nn'] - df['x_observed_tpf'])**2 +
-                                  (df['y_observed_nn'] - df['y_observed_tpf'])**2 < 6) &
+                                   (df['y_observed_nn'] - df['y_observed_tpf'])**2 < 6) &
                                   (df['r_observed_nn']**2 - df['r_observed_tpf']**2 > -80) &
                                   (df['r_observed_nn']**2 - df['r_observed_tpf']**2 < 140))
 
