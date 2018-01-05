7fe317f4f4e5b7d577ab70a8b3535e5bb764aa6b
Patrick de Perio <pdeperio@gmail.com>
2018-01-05 14:51:56 -0500

    Bump version: 1.2.3 → 1.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='lax',
-    version='1.2.3',
+    version='1.3.0',
     description="Package for standardizing event selections on hax minitrees.",
     long_description=readme + '\n\n' + history,
     author="Christopher Tunnell",
