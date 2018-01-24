8f7ba09f1b79b9138acc3da67d94ca2227352d29
Shingo Kazama <kazama@physik.uzh.ch>
2018-01-24 18:01:24 +0100

    Update sciencerun0.py

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -500,8 +500,7 @@ class S1PatternLikelihood(Lichen):
     version = 3
     def _process(self, df):
         s1t = df['s1']*df['s1_area_fraction_top']
-        s1b = df['s1']*(1.-df['s1_area_fraction_top'])
-      
+        s1b = df['s1']*(1.-df['s1_area_fraction_top'])      
         df.loc[:, self.name()] = ((df['s1_pattern_fit_hax']-df['s1_pattern_fit_bottom_hax']
                                    < 13.0 + 2.3*s1t**0.5 + 8.0*s1t - 1.0*s1t**1.5 + 0.04*s1t**2.0) &
                                   (df['s1_pattern_fit_bottom_hax']
