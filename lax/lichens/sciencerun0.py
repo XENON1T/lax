8d8f29fcd03f9209d6b4711e19bd9fb9b29171c5
Fei Gao <feigao.ge@gmail.com>
2018-01-26 20:43:20 -0600

    working version by individual checks

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -886,22 +886,20 @@ class SingleElectronS2s(Lichen):
 
     version = 0
 
-    # Random forest classifier
-    forest_filename = os.path.join(DATA_DIR, 'XENON1T_random_forest_peak_classifier_01262018.pkl')
-    forest_load = pickle.load(open(forest_filename, 'rb'))
-
-    # Gradient Boosted Decesion Tree classifier
-    gbdt_filename = os.path.join(DATA_DIR, 'XENON1T_gradient_bdt_peak_classifier_01262018.pkl')
-    gbdt_load = pickle.load(open(gbdt_filename, 'rb'))
-
-    def _classifier_soft(X):
-        return 0.5 * forest_load.predict_proba(X) + 0.5 * gbdt_load.predict_proba(X)
-
     def _process(self, df):
-        df['ses2prob'] = classifier_soft(df_s1_cl[['s1','s1_area_fraction_top','s1_rise_time', 's1_range_90p_area']])[:,1]
+        # Random forest classifier
+        forest_filename = os.path.join(DATA_DIR, 'XENON1T_random_forest_peak_classifier_01262018.pkl')
+        forest_load = pickle.load(open(forest_filename, 'rb'))
+
+        # Gradient Boosted Decesion Tree classifier
+        gbdt_filename = os.path.join(DATA_DIR, 'XENON1T_gradient_bdt_peak_classifier_01262018.pkl')
+        gbdt_load = pickle.load(open(gbdt_filename, 'rb'))
+
+        def _classifier_soft(X):
+            return 0.5 * forest_load.predict_proba(X) + 0.5 * gbdt_load.predict_proba(X)
+
+        df['ses2prob'] = _classifier_soft(df[['s1','s1_area_fraction_top','s1_rise_time', 's1_range_90p_area']])[:,1]
         cut_threshold = 0.7
         # current model is trained by data with S1 < 70PE and S1 width < 450PE
-        df.loc[:, self.name()] = (((df['ses2prob'] <= cut_threshold)) |
-                                   (df['s1_range_90p_area'] < 450) |
-                                   (df['s1'] > 70))
+        df.loc[:, self.name()] = (((df['ses2prob'] <= cut_threshold) & (df['s1_range_90p_area'] < 450)) | (df['s1'] > 70))
         return df
