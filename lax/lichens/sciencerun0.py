dc8c691d7e8355306e02ac15b5305d09b5c38aba
Patrick de Perio <pdeperio@users.noreply.github.com>
2018-02-27 15:11:01 -0500

    Update FV cut with fixed ER scaling and smoothing (#146)
    * Update FV cut with fixed ER scaling and smoothing
    
    * Increment version
    
    * Ignore last 3 top points in line fit
    
    since surface domination pushes threshold up
    (based on https://github.com/XENON1T/SR1Results/commit/e91673d4bb2eabfbe9097e71659bc26cf1d290b7)

--- lax/lichens/sciencerun0.py
+++ lax/lichens/sciencerun0.py
@@ -260,12 +260,12 @@ class FiducialZOptimized(StringLichen):
     This first version is a bit rough and may be improved by understanding the underlying BG KDE
     and errors better.
 
-    https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:sciencerun1:summary_fiducial_volume_v2
+    https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:sciencerun1:summary_fiducial_volume_v4
     """
-    version = 1
-    string = "(-95 < z_3d_nn) & (z_3d_nn < -8) & (r_3d_nn < 42.8387) & \
-              (z_3d_nn < 5.71966 - 0.0149755*r_3d_nn*r_3d_nn) & \
-              (z_3d_nn > -157.587 + 0.0405219*r_3d_nn*r_3d_nn)"
+    version = 4
+    string = "(-94 < z_3d_nn) & (z_3d_nn < -8) & (r_3d_nn < 42.8387) & \
+              (z_3d_nn < -2.63725 - 0.00946597*r_3d_nn*r_3d_nn) & \
+              (z_3d_nn > -158.173 + 0.0456094*r_3d_nn*r_3d_nn)"
 
 
 FV_CONFIGS = [
