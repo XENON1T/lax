5a1bbf28882cae07c448772bae6f45445e7beb00
Jelle Aalbers <JelleAalbers@users.noreply.github.com>
2018-06-01 17:20:09 +0200

    Create post-sr1 lichen file (#149)
    * Prepare post-sr1 lichen file
    
    * Probably futile attempt to make codacy happy

---
+++ lax/lichens/postsr1.py
@@ -0,0 +1,37 @@
+"""Cuts for post-SR1 analyses.
+
+The SR1 cuts are copied, unless they are overriden below.
+"""
+import inspect
+
+import numpy as np
+
+import lax
+from lax.lichen import ManyLichen, StringLichen  # pylint: disable=unused-import
+from lax import __version__ as lax_version
+
+from lax.lichens import sciencerun1 as sr1
+DATA_DIR = sr1.DATA_DIR
+
+# Import all lichens from sciencerun1
+for x in dir(sr1):
+    y = getattr(sr1, x)
+    if inspect.isclass(y) and issubclass(y, lax.lichen.Lichen):
+        locals()[x] = y
+
+##
+# Put new lichens here
+##
+
+class ERBandDEC(StringLichen):
+    """A cut used in the double electron capture analysis to preselect the ER band
+    Copied from xenon:xenon1t:analysis:backgrounds:ambe:fieguth:dec1t_final_analysis:cuts_overview
+
+    Do NOT use at low energies (cS1 <200 PE)
+    """
+    version = 1
+    string = "1 < log_cs_ratio < 2"
+
+    def pre(self, df):
+        df.loc[:, 'log_cs_ratio'] = np.log10(df['cs2']/df['cs1'])
+        return df
