3b31882c78dd11a344e97e10e72381c24d4a2b2c
Jingqiang Ye <yejingqiang1992@gmail.com>
2018-10-07 04:30:34 -0700

    Add S2PatternLikelihoodExtended Cut (#153)
    * Add S2PatternLikelihoodExtended Cut
    
    As presented in last Friday cut telecon, S2 PatternLikelihood Cut in SR1 is not suitable for high energy events, and a new cut is proposed for s2 up to 1.5e5 PE. The details can be found in this [note](https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:jingqiang:darkphoton:s2patternlikelihood_200kev)
    
    * Update S2PLH cut name
    
    * Update postsr1.py

--- lax/lichens/postsr1.py
+++ lax/lichens/postsr1.py
@@ -37,6 +37,28 @@ class ERBandDEC(StringLichen):
         return df
 
 
+class S2PatternLikelihood(StringLichen):
+    """
+    Extend S2 PatternLikelihood(S2 PLH) Cut up to 1.5e5 PE S2, which is good for up to around 220 keVee. This cut is a
+    combination of SR1 S2PLH(s2 < 10000 PE) and extension of S2 PLH(1e4 < S2 < 1.5e5 PE), thus have same performance as 
+    sr1 S2 PLH for low energy and have improved performance for high energy. S2 PLH cut aims to remove poorly 
+    reconstructed events.
+
+    The details can be found below:
+    https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:jingqiang:darkphoton:s2patternlikelihood_200kev
+
+    Requires Extended minitrees.
+    Contact: Jingqiang Ye <jiy171@ucsd.edu>
+    """
+    version = 0
+    p0 = (0.072, 594)
+    p1_sr1 = (0.0404, 594, 0.0737, -686)
+
+
+    string = ("((s2_pattern_fit < %.3f*s2 + %.3f) & (s2 > 10000))\
+             | ((s2_pattern_fit < %.3f*s2 + %.3f*s2**%.3f + %.3f) & (s2 < 10000))" %(p0 + p1_sr1))
+
+
 class CS2AreaFractionTopExtended(StringLichen):
     """"An extension of CS2AreaFractionTop to the entire S2 range
     with a designed acceptance of 99%.
