2a360efbd68a661e4cd8e478a7450d0b1d3b1290
Dominick Cichon <Dominick.Cichon@mpi-hd.mpg.de>
2018-10-07 13:27:55 +0200

    Addition of CS2AreaFractionTopExtended (#152)
    * Added CS2AreaFractionTopExtended.
    
    * Updated CS2AreaFractionTopExtended lichen string.
    
    * Fixed syntax error.

--- lax/lichens/postsr1.py
+++ lax/lichens/postsr1.py
@@ -35,3 +35,40 @@ class ERBandDEC(StringLichen):
     def pre(self, df):
         df.loc[:, 'log_cs_ratio'] = np.log10(df['cs2']/df['cs1'])
         return df
+
+
+class CS2AreaFractionTopExtended(StringLichen):
+    """"An extension of CS2AreaFractionTop to the entire S2 range
+    with a designed acceptance of 99%.
+    It is defined in the (cxys2, cs2_aft) space, with:
+
+    cxys2 = (cs2_top + cs2_bottom) / s2_lifetime_correction
+    cs2_aft = cs2_top / (cs2_top + cs2_bottom)
+
+    Events where cxys2 > 1922700 PE are not cut.
+
+    Required minitrees: Corrections
+    Defined with pax version: 6.8.0
+    Wiki note: xenon:xenon1t:double_beta:cs2_aft_extension
+    Contact: Dominick Cichon <dominick.cichon@mpi-hd.mpg.de>"""
+
+    version = 0
+
+    top_bound_string = ('(6.480304E-01 + 1.445442E-07 * cxys2 +'
+                        ' -4.443394E-13 * cxys2**2 + 5.440908E-19 * cxys2**3 +'
+                        ' -2.925384E-25 * cxys2**4 + 5.716877E-32 * cxys2**5 +'
+                        ' 1.060000E+00 / sqrt(cxys2) + 5.297425E+00 / cxys2)')
+    bot_bound_string = ('(6.135743E-01 + 3.979536E-08 * cxys2 +'
+                        ' -8.288567E-14 * cxys2**2 + 2.617481E-20 * cxys2**3 +'
+                        ' -5.108758E-27 * cxys2**4 + 2.312916E-33 * cxys2**5 +'
+                        ' -6.921489E-01 / sqrt(cxys2) + -2.507866E+01 / cxys2)')
+
+    string = ('((' + top_bound_string + ' > cs2_aft) & (' + bot_bound_string +
+              ' < cs2_aft)) | cxys2 > 1922700')
+
+    def pre(self, df):
+        df.loc[:, 'cxys2'] = ((df['cs2_top'] + df['cs2_bottom']) /
+                                df['s2_lifetime_correction'])
+        df.loc[:, 'cs2_aft'] = df['cs2_top'] / (df['cs2_top'] +
+                                                df['cs2_bottom'])
+        return df
