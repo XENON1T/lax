a2e85e6e5830335ff4b87cb20ef75b558c2a6b80
JelleAalbers <jelleaalbers@gmail.com>
2019-05-10 14:17:32 -0500

    Bump version: 1.7.1 → 1.7.2

--- lax/__init__.py
+++ lax/__init__.py
@@ -1,5 +1,5 @@
 """Module defining key components to lax"""
 
-__version__ = '1.7.1'
+__version__ = '1.7.2'
 
 from . import lichens
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='lax',
-    version='1.7.1',
+    version='1.7.2',
     description="Package for standardizing event selections on hax minitrees.",
     long_description=readme + '\n\n' + history,
     author="Christopher Tunnell",
