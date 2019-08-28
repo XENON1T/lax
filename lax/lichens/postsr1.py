c258a7b4af959a95b7ad32e996df73085d412c42
ctherreau <chloe.therreau@subatech.in2p3.fr>
2019-08-28 03:49:33 -0500

    update

--- lax/lichens/postsr1.py
+++ lax/lichens/postsr1.py
@@ -25,6 +25,7 @@ for x in dir(sr1):
 # Put new lichens here
 ##
 
+
 class ERBandDEC(StringLichen):
     """A cut used in the double electron capture analysis to preselect the ER band
     Copied from xenon:xenon1t:analysis:backgrounds:ambe:fieguth:dec1t_final_analysis:cuts_overview
@@ -41,10 +42,10 @@ class ERBandDEC(StringLichen):
 
 class S2PatternLikelihood(StringLichen):
     """
-    Extend S2 PatternLikelihood(S2 PLH) Cut up to 1.5e5 PE S2, which is good for up to around 220 keVee. This cut is a
-    combination of SR1 S2PLH(s2 < 10000 PE) and extension of S2 PLH(1e4 < S2 < 1.5e5 PE), thus have same performance as 
-    sr1 S2 PLH for low energy and have improved performance for high energy. S2 PLH cut aims to remove poorly 
-    reconstructed events.
+    Extend S2 PatternLikelihood(S2 PLH) Cut up to 1.5e5 PE S2, which is good for up to around 220 keVee. 
+    This cut is a combination of SR1 S2PLH(s2 < 10000 PE) and extension of S2 PLH(1e4 < S2 < 1.5e5 PE),
+    thus have same performance as sr1 S2 PLH for low energy and have improved performance for high energy.
+    S2 PLH cut aims to remove poorly reconstructed events.
 
     The details can be found below:
     https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:jingqiang:darkphoton:s2patternlikelihood_200kev
