5a543faf990722d370a087d4d2044fce6e5952e4
Giovanni Volta <gvolta@dali-login1.rcc.local>
2019-12-03 04:28:48 -0600

    & -> |

--- lax/lichens/postsr1.py
+++ lax/lichens/postsr1.py
@@ -315,7 +315,7 @@ class S1PatternLikelihood_HE(Lichen):
         cut_z_2   = (df['s1_pattern_fit_bottom_hax'] > self.cutline_z_2(df['z_3d_nn_tf']))
         cut_S1_1 = ((df['s1_pattern_fit_bottom_hax'] < self.cutline_S1_1(df['s1']))&(df['s1']<self.S1_thr))
         cut_S1_2 = ((df['s1_pattern_fit_bottom_hax'] < self.cutline_S1_2(df['s1']))&(df['s1']>=self.S1_thr))
-        cut = (cut_z_1&cut_z_2)&(cut_S1_1|cut_S1_2)
+        cut = (cut_z_1&cut_z_2)|(cut_S1_1|cut_S1_2)
         df.loc[:, self.name()] = cut
         return df
     
