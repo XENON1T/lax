18933439ed2b44b5897671f7017a5356e4dadc82
Giovanni Volta <38431109+GiovanniVolta@users.noreply.github.com>
2019-12-18 15:07:26 +0100

    Update postsr1.py
    S1 pattern likelihood cut for HE

--- lax/lichens/postsr1.py
+++ lax/lichens/postsr1.py
@@ -343,3 +343,42 @@ class S2Width_HE(Lichen):
 
     def g2_sr1_he_ap(self, z):
         return 10.504-(0.015*z)
+
+class S1PatternLikelihood_HE(Lichen):
+    """
+    This cut is meant to cut Gamma-X events and to help the rejection of multiple scatter evens. Moreover it is sensitive
+    also othe anomalies: unexpected S1 pattern, S1 and S2 not corresponding to the same interaction, ...
+    Note: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:giovo:s1patternlikelihood_he
+    Contact: gvolta@physik.uzh.ch
+    """
+    version = 0.1
+    popt_z_1 =  [2.38811218e+02, 2.55991432e-05, 1.89468970e-01] #exp
+    popt_z_2 =  [1.05256551e+02, 7.72450878e-02] #poly1
+    popt_S1_1 = [1.49406369e+01,  2.62994597e+01, -1.01825116e+00,  1.27941177e-02] #cutline_S1_1
+    popt_S1_2 = [2.18914476e+02, 1.19392164e+02, 5.32460349e-05] #cutline_S1_2
+    S1_thr = 600
+    # Function for cut definition in z
+    def cutline_z_1(self, x):
+        return self.popt_z_1[0] + self.popt_z_1[1]*np.exp(-self.popt_z_1[2]*x)
+    # Function for cut definition in z
+    def cutline_z_2(self, x):
+        return self.popt_z_2[0] + self.popt_z_2[1]*x
+    # Function for cut definition below 600 PE
+    def cutline_S1_1(self, x):
+        return self.popt_S1_1[0] + self.popt_S1_1[1]*pow(x, 0.5) + self.popt_S1_1[2]*x + self.popt_S1_1[3]*pow(x, 1.5)
+    # Function for cut definition above 600 PE
+    def cutline_S1_2(self, x):
+        return self.popt_S1_2[0] + self.popt_S1_2[1]*np.arctan(self.popt_S1_2[2]*x)
+
+    def _process(self, df):
+        S1PL = df['s1_pattern_fit_bottom_hax']
+        z = df['z_3d_nn_tf']
+        S1 = df['s1']
+
+        cut_z_1   = (df['s1_pattern_fit_bottom_hax'] < self.cutline_z_1(df['z_3d_nn_tf']))
+        cut_z_2   = (df['s1_pattern_fit_bottom_hax'] > self.cutline_z_2(df['z_3d_nn_tf']))
+        cut_S1_1 = ((df['s1_pattern_fit_bottom_hax'] < self.cutline_S1_1(df['s1']))&(df['s1']<self.S1_thr))
+        cut_S1_2 = ((df['s1_pattern_fit_bottom_hax'] < self.cutline_S1_2(df['s1']))&(df['s1']>=self.S1_thr))
+        cut = (cut_z_1&cut_z_2)&(cut_S1_1|cut_S1_2)
+        df.loc[:, self.name()] = cut
+        return df    
